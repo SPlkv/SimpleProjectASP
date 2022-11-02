48c975d77dac371d1fbec185a567c7e864a240a0
SPlkv <polkova01@mail.ru>
2022-11-02 11:34:31 +0500

    SiimpleProjectASP

---
+++ HelloProj/HelloProj/Controllers/ContactsController.cs
@@ -0,0 +1,23 @@
+﻿using HelloProj.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloProj.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()    //contacts
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact)    //contacts
+        {
+            if(ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            return View("Index");
+        }
+    }
+}
